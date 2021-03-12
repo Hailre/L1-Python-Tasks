43ee94e49982aca6ee1a6bbeec6ade5684249d28
Hailre <reeceh@stu.otc.school.nz>
2021-03-12 13:24:53 +1300

    Level 1 Python

--- Gift_Card_Task.py
+++ Gift_Card_Task.py
@@ -1,4 +1,5 @@
 Balance = 50
 game_extension_pack = 35
 print(f"Balance ${Balance}")
-print(f"Your new balance is ${Balance - game_extension_pack}")
+print(f"Item Cost ${game_extension_pack}")
+print(f"From this purchase your new balance is ${Balance - game_extension_pack}")
