67730983efee21c4ae55f4c2512c534fa93c13e7
Hailre <reeceh@stu.otc.school.nz>
2021-03-12 13:12:06 +1300

    Level 1 Python

--- Task_1.py
+++ Task_1.py
@@ -1,10 +1,15 @@
+#Points Scored in 5 matches
 Match_1 = 3
 Match_2 = 6
 Match_3 = 10
 Match_4 = 10
 Match_5 = 6
 
-Total_points = Match_1 + Match_2 + Match_3 + Match_4 + Match_5 (?)
+Total_points = Match_1 + Match_2 + Match_3 + Match_4 + Match_5
 
-print(f"First Match = {Match_1} , Second Match = {Match_2} , Third Match = {Match_3} , Fourth Match {Match_4} , Fifth Match {Match_5}")
+print(f"First Match = {Match_1}"
+      f" Second Match = {Match_2}"
+      f" Third Match = {Match_3}"
+      f" Fourth Match = {Match_4}"
+      f" Fifth Match = {Match_5}")
 print(f"The total match points were: {Total_points}")
