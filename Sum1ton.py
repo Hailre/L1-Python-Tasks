f5b1fd257baff7046ed9ee90064d82099c6ff466
Hailre <reeceh@stu.otc.school.nz>
2021-03-23 15:01:29 +1300

    Level 1 Python

--- Sum1ton.py
+++ Sum1ton.py
@@ -1,5 +1,3 @@
 number = int(input("Pick a number to count to: "))
 
-while number >= 1:
-    print(f"The factorial of that number is {(number +1) * number/2}")
-i += 1
+print(f"The factorial of that number is {(number +1) * number/2}")
