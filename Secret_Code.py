3ef996eddd1f3229e729decabc2c9f03bb9bdd4f
Hailre <reeceh@stu.otc.school.nz>
2021-03-26 13:22:56 +1300

    Level 1 Python

--- Secret_Code.py
+++ Secret_Code.py
@@ -1,6 +1,7 @@
 pin_number = int(input("Please enter your pin number: "))
 i = 0
 while i < pin_number:
+#.zfill(4) - fills code to 4 digit places
     print(str(i).zfill(4))
     i += 1
 pin = str(i).zfill(4)
